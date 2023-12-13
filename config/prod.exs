de01915c630e5b103d9783b8423b12021439906d
franknfjr <frankdonascimentojr@gmail.com>
2023-12-13 15:55:51 -0300

    fix cors

--- config/prod.exs
+++ config/prod.exs
@@ -6,7 +6,10 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :frank_ferreira, FrankFerreiraWeb.Endpoint,
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  check_origin: [
+    "//*.frankferreira.dev"
+  ]
 
 # Do not print debug messages in production
 config :logger, level: :info
